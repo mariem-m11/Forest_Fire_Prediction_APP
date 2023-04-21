4ae91ab6d729017334793fe7f4227c500e09fa7f
Meruem <70331420+mariem-m11@users.noreply.github.com>
2023-04-21 21:37:19 +0100

    Create test_app.py

---
+++ test_app.py
@@ -0,0 +1,29 @@
+import pytest
+import httpx
+from fastapi.testclient import TestClient
+from FastAPI_backend.main import app, ModelInput
+
+
+client = TestClient(app)
+
+#successful prediction
+def test_successful_prediction():
+    test_data = {
+        "temp": 25.0,
+        "Ws": 13.0,
+        "Rain": 2.5,
+        "FFMC": 28.6,
+        "DMC": 1.3,
+        "ISI": 0.0
+    }
+    response = client.post("/prediction", json=test_data)
+    assert response.status_code == 200
+    assert response.json()["prediction value"] in [0, 1]
+
+#failed prediction due to missing data
+@pytest.mark.asyncio
+async def test_smoke():
+    async with httpx.AsyncClient(app=app, base_url="http://test") as ac:
+        response = await ac.get("/smoke")
+        assert response.status_code == 200
+        assert response.json() == {"status": "OK"}
